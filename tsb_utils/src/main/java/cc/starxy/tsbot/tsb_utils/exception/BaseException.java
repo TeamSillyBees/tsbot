deddb534865e9ec18c55b081f4525dd545473b89
Starxy <galaxy_djx@hotmail.com>
2020-09-10 16:23:23 +0800

    add custom exception

---
+++ tsb_utils/src/main/java/cc/starxy/tsbot/tsb_utils/exception/BaseException.java
@@ -0,0 +1,24 @@
+package cc.starxy.tsbot.tsb_utils.exception;
+
+/**
+ * 用户自定义异常
+ *
+ * @author DONG Jixing
+ */
+public abstract class BaseException extends RuntimeException {
+    private String message;
+
+    public BaseException() {
+        super();
+    }
+
+    public BaseException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
