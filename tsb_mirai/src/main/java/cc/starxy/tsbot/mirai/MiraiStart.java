6ca37d5b48fe21228de96c52663bb1279fdc56db
Starxy <galaxy_djx@hotmail.com>
2020-09-10 18:15:24 +0800

    fix: make group and fried listener inject by spring ioc

--- tsb_mirai/src/main/java/cc/starxy/tsbot/mirai/MiraiStart.java
+++ tsb_mirai/src/main/java/cc/starxy/tsbot/mirai/MiraiStart.java
@@ -2,6 +2,7 @@ package cc.starxy.tsbot.mirai;
 
 import cc.starxy.tsbot.mirai.core.MiraiCore;
 import cc.starxy.tsbot.mirai.listener.FriendListener;
+import cc.starxy.tsbot.mirai.listener.GroupListener;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.event.Events;
 
@@ -13,15 +14,24 @@ import net.mamoe.mirai.event.Events;
  */
 public class MiraiStart {
 
-    public static void start(Long miraiUsername, String miraiPassword,Long notifyGroup) {
+    /**
+     * mirai 启动
+     * @param miraiUsername bot qq
+     * @param miraiPassword bot pwd
+     * @param notifyGroup 通知群号
+     * @param groupListener 群组消息监听器
+     * @param friendListener 私聊消息监听器
+     */
+    public static void start(Long miraiUsername, String miraiPassword, Long notifyGroup,
+                             GroupListener groupListener, FriendListener friendListener) {
 
 
         // 设置报错群组
         MiraiCore.getInstance().setErrorNotifyGroup(notifyGroup);
         // Mirai login
         Bot bot = MiraiCore.getInstance().login(miraiUsername, miraiPassword);
-//        Events.registerEvents(bot, new GroupListener());
-        Events.registerEvents(bot, new FriendListener());
+        Events.registerEvents(bot, groupListener);
+        Events.registerEvents(bot, friendListener);
 
         // 挂载协程
         bot.join();
--- tsb_starter/src/main/java/cc/starxy/tsbot/starter/MiraiBotApplication.java
+++ tsb_starter/src/main/java/cc/starxy/tsbot/starter/MiraiBotApplication.java
@@ -1,6 +1,8 @@
 package cc.starxy.tsbot.starter;
 
 import cc.starxy.tsbot.mirai.MiraiStart;
+import cc.starxy.tsbot.mirai.listener.FriendListener;
+import cc.starxy.tsbot.mirai.listener.GroupListener;
 import cc.starxy.tsbot.starter.config.MiraiConfig;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * Mirai Bot start bean
+ *
  * @author DONG Jixing
  */
 @Component
@@ -15,12 +18,18 @@ public class MiraiBotApplication implements ApplicationRunner {
 
     private final MiraiConfig mirai;
 
-    public MiraiBotApplication(MiraiConfig mirai) {
+    private final GroupListener groupListener;
+
+    private final FriendListener friendListener;
+
+    public MiraiBotApplication(MiraiConfig mirai, GroupListener groupListener, FriendListener friendListener) {
         this.mirai = mirai;
+        this.groupListener = groupListener;
+        this.friendListener = friendListener;
     }
 
     @Override
     public void run(ApplicationArguments args) {
-        MiraiStart.start(mirai.getBotId(), mirai.getPassword(),mirai.getNotifyGroupId());
+        MiraiStart.start(mirai.getBotId(), mirai.getPassword(), mirai.getNotifyGroupId(), groupListener, friendListener);
     }
 }
